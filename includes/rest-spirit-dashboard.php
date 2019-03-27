55d542933cb990273311a38245f9ccedb09722c5
vpsnak <info.vpsnak@gmail.com>
2019-03-27 17:34:22 +0200

    - API Tweaks

--- includes/rest-spirit-dashboard.php
+++ includes/rest-spirit-dashboard.php
@@ -74,13 +74,82 @@ add_action('rest_api_init', function () {
 
     function get_app_data()
     {
-        $data_response['wordpress'] = get_site_transient('update_core');
-        $data_response['plugins'] =  get_site_transient('update_plugins');
-        $data_response['themes'] = get_site_transient('update_themes');
-
-        $data_response['server'] = array(
-            'php_version' => phpversion()
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $data_response['info'] = array(
+            'name' => get_bloginfo('name'),
+            'url' => get_bloginfo('url'),
+            'logo' => wp_get_attachment_image_src($custom_logo_id, 'full')[0],
         );
+        $update_core = get_site_transient('update_core');
+        $data_response['wordpress'] = array(
+            'current_version' => $update_core->updates[0]->current,
+            'latest_version' => $update_core->updates[0]->version,
+            'php_version' => phpversion(),
+            'last_check' => date('d-m-Y H:m', $update_core->last_checked),
+        );
+
+        if (!function_exists('get_plugins')) {
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
+
+        $all_plugins = get_plugins();
+        $data_response['plugins']['count'] = count($all_plugins);
+        $update_plugins = get_site_transient('update_plugins');
+        $plugin_update = $update_plugins->response;
+        if ($plugin_update) {
+            $data_response['plugins']['update']['count'] = count($plugin_update);
+            foreach ($plugin_update as $key => $plugin) {
+                $data_response['plugins']['update'][$key] = array(
+                    'name' => $all_plugins[$key]['Name'],
+                    'url' => $plugin->url,
+                    'icons' => $plugin->icons,
+                    'plugin_uri' => $all_plugins[$key]['PluginURI'],
+                    'author' => $all_plugins[$key]['Author'],
+                    'author_uri' => $all_plugins[$key]['AuthorURI'],
+                    'current_version' => $all_plugins[$key]['Version'],
+                    'latest_version' => $plugin->new_version,
+                );
+            }
+        } else {
+            $data_response['plugins']['update']['count'] = 0;
+        }
+
+        $plugin_no_update = $update_plugins->no_update;
+        if ($plugin_no_update) {
+            $data_response['plugins']['no_update']['count'] = count($plugin_no_update);
+            foreach ($plugin_no_update as $key => $plugin) {
+                $data_response['plugins']['no_update'][$key] = array(
+                    'name' => $all_plugins[$key]['Name'],
+                    'url' => $plugin->url,
+                    'icons' => $plugin->icons,
+                    'plugin_uri' => $all_plugins[$key]['PluginURI'],
+                    'author' => $all_plugins[$key]['Author'],
+                    'author_uri' => $all_plugins[$key]['AuthorURI'],
+                    'current_version' => $all_plugins[$key]['Version'],
+                    'latest_version' => $plugin->new_version,
+                );
+            }
+        } else {
+            $data_response['plugins']['no_update']['count'] = 0;
+        }
+
+        $update_themes = get_site_transient('update_themes');
+        $checked_themes = $update_themes->checked;
+        $themes_update = $update_themes->response;
+        $data_response['themes']['count'] = count($checked_themes);
+        if ($themes_update) {
+            $data_response['themes']['update']['count'] = count($themes_update);
+            foreach ($themes_update as $key => $theme) {
+                $data_response['themes']['update'][$key] = array(
+                    'name' => $theme['theme'],
+                    'url' => $theme['url'],
+                    'current_version' => $checked_themes[$key],
+                    'latest_version' => $theme['new_version'],
+                );
+            }
+        } else {
+            $data_response['themes']['update']['count'] = 0;
+        }
 
         return ($data_response);
     }
